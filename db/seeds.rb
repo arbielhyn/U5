05f2597627354398562ccc2b91417069be9a1db3
arbielhyn <150289854+arbielhyn@users.noreply.github.com>
2024-06-10 12:34:58 -0500

    Rewrite seeds file to load data from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,27 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 require "csv"
+require "csv"
+
+Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  category_name = product['category']
+  category = Category.find_or_create_by(name: category_name)
+
+  Product.create(
+    title: product['title'],
+    description: product['description'],
+    price: product['price'],
+    stock_quantity: product['stock_quantity'],
+    category: category
+  )
+end
 
 676.times do
     Product.create(
