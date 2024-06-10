c6724b5433714abb8c544d6515d635b432146472
arbielhyn <150289854+arbielhyn@users.noreply.github.com>
2024-06-10 12:30:17 -0500

    Category model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :products, only: [:index, :show]
-  get 'products/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Set the root route to products#index
+  root 'products#index'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # You can remove or uncomment this line if you have other controllers or actions to define.
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
